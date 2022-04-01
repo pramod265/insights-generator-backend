cbfb83ae5ebb0665c9c6fd3852d673b187f980bb
Pramod <pga286478@gmail.com>
2022-04-01 22:32:59 +0530

    moved pandas profilg to wsgi file

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,13 @@
 from main import app
+import pip
 
 if __name__ == "__main__":
+
+    if hasattr(pip, 'main'):
+        pip.main(['install', 'pandas-profiling'])
+    else:
+        pip._internal.main(['install', 'pandas-profiling'])
+    
     app.debug=True
     app.run()
 
